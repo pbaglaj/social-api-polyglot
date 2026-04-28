c3ce6b57534ee1c504e6afa3cb4ae4f4450e3add
Pawel Baglaj <184997716+pbaglaj@users.noreply.github.com>
2026-04-28 23:28:48 +0200

    fix routes for posts and fix file path in docker

--- backend/pg-service/src/postRoutes.ts
+++ backend/pg-service/src/postRoutes.ts
@@ -1,4 +1,4 @@
-import e, { Router } from 'express';
+import  { Router } from 'express';
 import type { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
 
